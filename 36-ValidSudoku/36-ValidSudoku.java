ef8785b2f2f74100b0c14d0fe899a01c2c23b1a7
drake2118 <88421237+drake2118@users.noreply.github.com>
2024-07-29 01:02:40 +0530

    Create file

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if (board.length != 9)
+            return false;
+        boolean[][] row_seen = new boolean[9][9], col_seen = new boolean[9][9];
+        boolean[][][] small_seen = new boolean[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            if (board[i].length != 9) return false;
+            for (int j = 0; j < 9; j++) {
+                char digit = board[i][j];
+                if (digit == '.') continue;
+                if (!Character.isDigit (digit) || digit == '0') return false;
+                int index = digit - '0' - 1;
+                if (row_seen[i][index]) return false;
+                row_seen[i][index] = true;
+                if (col_seen[j][index]) return false;
+                col_seen[j][index] = true;
+                int x = i / 3, y = j / 3;
+                if (small_seen[x][y][index]) return false;
+                small_seen[x][y][index] = true;
+            }
+        }
+        return true;
+    }
+}
