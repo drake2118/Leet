1526b9bfb1accad3a0d467ce4611f4c7d1d8911b
drake2118 <88421237+drake2118@users.noreply.github.com>
2024-07-30 22:56:03 +0530

    Create file

---
+++ 1653-MinimumDeletionstoMakeStringBalanced/1653-MinimumDeletionstoMakeStringBalanced.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int i = 0;
+        for(i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='b')
+                break;
+        }
+        int countflips = 0;
+        int countb = 0;
+        for(i=i; i < s.length(); i++){
+            if(s.charAt(i)=='a') countflips++;
+            
+            else countb++;
+            
+            countflips = Math.min(countflips, countb);
+        }    
+        return countflips;
+    }
+}
