be2736b1c07b896a3fbde422e13210cdf043a115
drake2118 <88421237+drake2118@users.noreply.github.com>
2024-08-04 23:54:53 +0530

    Create file

---
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0,right=numbers.length-1;
+        int [] res=new int[2];
+        while(left<right){
+            if(numbers[left]+numbers[right]<target) left++;
+            if(numbers[left]+numbers[right]>target) right--;
+            if(numbers[left]+numbers[right]==target) break;
+        }
+        res[0]=left+1;
+        res[1]=right+1;
+        return res;
+    }
+}
